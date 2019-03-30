2bb5341f53c9a0ab429f1c7bf2baa42607c1f4eb
Aron Katona <katonaaron01@gmail.com>
2019-03-30 20:19:38 +0200

    Initial commit

---
+++ PEParser/PEParser.c
@@ -0,0 +1,16 @@
+// PEParser.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        printf("error: A path to a PE file is required\n");
+        return -1;
+    }
+
+
+    return 0;
+}
